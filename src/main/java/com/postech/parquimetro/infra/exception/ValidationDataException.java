9a5bb02e603e532cb83a558a8f81935eb6dc4cbf
mendesmoe <m.mendes@depiltech.com>
2023-12-27 16:28:59 +0100

    feat:create-a-relationship-customer-vehicle

--- src/main/java/com/postech/parquimetro/infra/exception/ValidationDataException.java
+++
@@ -1,7 +0,0 @@
-package com.postech.parquimetro.infra.exception;
-
-public class ValidationDataException extends RuntimeException {
-    public ValidationDataException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/postech/parquimetro/repository/VehicleRepository.java
+++ src/main/java/com/postech/parquimetro/repository/VehicleRepository.java
@@ -2,6 +2,8 @@ package com.postech.parquimetro.repository;
 
 import com.postech.parquimetro.domain.vehicle.Vehicle;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VehicleRepository extends MongoRepository<Vehicle, String> {
 }
