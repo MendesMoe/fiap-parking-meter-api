72c7403b29514d8aefeddad007f40ef7e4d1e48e
mendesmoe <m.mendes@depiltech.com>
2024-01-22 14:47:13 +0100

    fix:security-configurations-route-customer

--- src/main/java/com/postech/parquimetro/infra/security/SecurityConfigurations.java
+++ src/main/java/com/postech/parquimetro/infra/security/SecurityConfigurations.java
@@ -13,8 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
@@ -28,9 +26,13 @@ public class SecurityConfigurations {
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/login", "/customer","/customer/*").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources/**", "/webjars/**").permitAll()
-                        .anyRequest().authenticated());
+                        .requestMatchers("/login", "/customer",
+                                "/customer/*","/swagger-ui/**", "/v3/api-docs",
+                                "/v3/api-docs/**", "/swagger-resources/**",
+                                "/webjars/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated());
 
         return http.build();
     }
